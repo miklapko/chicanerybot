f3f55fe889d3ac294b4a708ab4875129ed4180c7
Mikalai.Lapko <Mikalai_Lapko@epam.com>
2022-08-13 23:06:03 +0200

    Yapp

--- bot.py
+++ bot.py
@@ -2,8 +2,9 @@ import praw
 import random
 import re
 
+
 def run_bot():
-    bot_message_disclaimer = """\n &nbsp; \n *** \n ^(*Beep boop. I am a bot, 
+    bot_message_disclaimer = """\n &nbsp; \n *** \n ^(*Beep boop. I am a bot,
     and this action was performed automatically.*)"""
     reddit = praw.Reddit('chicanerybot')
     subreddit = reddit.subreddit("okbuddychicanery")
@@ -17,13 +18,15 @@ def run_bot():
     for comment in subreddit.stream.comments():
         if comment.id not in replied_comments:
             if re.search("fuck", comment.body, re.IGNORECASE):
-                response = str(random.choice(['Yep!', 'Yup', 'Yup! Yup!', 'Yapp']))
+                response = str(random.choice(
+                    ['Yep!', 'Yup', 'Yup! Yup!', 'Yapp']))
                 comment.reply(response + bot_message_disclaimer)
                 replied_comments.append(comment.id)
 
                 with open("replied_comments.txt", "w") as f:
                     for comment_id in replied_comments:
                         f.write(comment_id + "\n")
+
 
 def main():
     run_bot()
